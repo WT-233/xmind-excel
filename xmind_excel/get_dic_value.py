d37f9c76aadd92d053e3cefcb6c555ef148a46d2
wt <wt>
2020-12-15 20:14:25 +0800

    12.15

--- xmind_excel/get_dic_value.py
+++ xmind_excel/get_dic_value.py
@@ -7,7 +7,7 @@ class GetDicValue(object):
     def __init__(self, error_text = None):
         self.error_text = error_text
 
-    def get_dic_key_is_title(self, i_dic):
+    def get_value_key_is_title(self, i_dic):
         """
         获取dic，key=title,的value
         :param value:
@@ -16,10 +16,10 @@ class GetDicValue(object):
 
         try:
             return i_dic['title']
-        except KeyError:
+        except Exception as e:
             return self.error_text
 
-    def get_dic_key_is_topic(self, i_dic):
+    def get_value_key_is_topic(self, i_dic):
         """
         获取dic，key=topic,的value
         :param value:
@@ -27,10 +27,10 @@ class GetDicValue(object):
         """
         try:
             return i_dic['topic']
-        except KeyError:
+        except Exception as e:
             return self.error_text
 
-    def get_dic_key_is_topics(self, i_dic):
+    def get_value_key_is_topics(self, i_dic):
         """
         获取dic，key=topics,的value
         :param value:
@@ -38,5 +38,7 @@ class GetDicValue(object):
         """
         try:
             return i_dic['topics']
-        except KeyError:
+        except Exception as e:
             return self.error_text
+if __name__ == '__main__':
+    pass
--- xmind_excel/xmind_excel_II.py
+++ xmind_excel/xmind_excel_II.py
@@ -22,7 +22,8 @@ from logging.handlers import TimedRotatingFileHandler
 from datetime import datetime
 import datetime
 import timeit
-from xmind_excel.get_dic_value import *
+
+from xmind_excel.get_dic_value import GetDicValue
 
 class GetXmindExcel(object):
     """
@@ -36,11 +37,21 @@ class GetXmindExcel(object):
         self.path = '/wuting'
 
         #计算测试用例的条数
-        self.row_num = 1
-        self.xmind_file_dic = {}
-        self.xmind_file_name_string = ""
+        # self.row_num = 1
+        # self.xmind_file_dic = {}
+        # self.xmind_file_name_dic = {}
 
-        self.GET_DIC_VALUE = get_dic_value()
+        self.GET_DIC_VALUE = GetDicValue()
+
+        self.xmind_dic = {}
+
+        self.leves = {
+            "leve0": "leve0",
+            "leve1": "leve0",
+            "leve2": "leve0",
+            "leve3": "leve0",
+            "leve4": "leve0"
+        }
 
     def get_xmind_json(self):
         """
@@ -49,9 +60,75 @@ class GetXmindExcel(object):
         """
 
         #获取到xmind的基本信息
-        xmind_file_dic = xmind_to_dict(self.xmind_name)
-        self.xmind_file_dic = self.GET_DIC_VALUE.get_dic_key_is_topic(xmind_file_dic)
-        self.xmind_file_name_string = self.get_xmind_title(self.xmind_file_dic)
+        xmind_file_dic = xmind_to_dict(self.xmind_name)[0]['topic']
+
+        xmind_title = self.GET_DIC_VALUE.get_value_key_is_title(xmind_file_dic)
+        xmind_topics = self.GET_DIC_VALUE.get_value_key_is_topics(xmind_file_dic)
+
+        #用列表的形式存放模块信息
+        moudle_names = []
+        moudle_topics = []
+        for i in xmind_topics:
+            moudle_names.append(self.GET_DIC_VALUE.get_value_key_is_title(i))
+            moudle_topics.append(self.GET_DIC_VALUE.get_value_key_is_topics(i))
+
+        for i in range(0, len(moudle_names)):
+            """
+            获取模块名称
+            """
+            moudle_name = moudle_names[i]
+            moudle_topics = moudle_topics[i]
+
+
+
+            for t in moudle_topics:
+                """
+                获取标题1—3、用例等级
+                """
+                title_names = ["","",""]
+                title_leve = ""
+
+                title_names[0] = self.GET_DIC_VALUE.get_value_key_is_title(t)
+                title_topics = self.GET_DIC_VALUE.get_value_key_is_topics(t)
+                if "makers" in title_topics[0].values():
+                    title_leve = title_topics[0]["makers"]
+
+                #获取标题2
+                title_names[1] = self.GET_DIC_VALUE.get_value_key_is_title(title_topics[0])
+                title_topics = self.GET_DIC_VALUE.get_value_key_is_topics(title_topics[0])
+
+                # 获取标题3
+                title_names[2] = self.GET_DIC_VALUE.get_value_key_is_title(title_topics[0])
+                title_topics = self.GET_DIC_VALUE.get_value_key_is_topics(title_topics[0])
+
+                for s in title_topics:
+                    """
+                    获取执行步骤、预期结果
+                    {'title': 'A1-1用例步骤1', 'topics': [{'title': 'A1-1预期结果1'}]}
+                    """
+                    title_step = []
+                    title_preset = []
+
+                    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         FeaturesNUM = self.numberLen(self.XmindContent)
